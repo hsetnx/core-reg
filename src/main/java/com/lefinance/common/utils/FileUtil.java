b4d4eb92ea89bf539367644408496c83ec5e0fcc
jingyan <jingyan@le.com>
2017-09-06 18:30:39 +0800

    NIO

--- src/main/java/com/lefinance/common/utils/FileUtil.java
+++ src/main/java/com/lefinance/common/utils/FileUtil.java
@@ -1,28 +1,37 @@
 package com.lefinance.common.utils;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 /**
  * @Author: jingyan
  * @Time: 2017/7/27 10:51
- * @Describe:
+ * @Describe: 文件工具类
  */
 public class FileUtil {
 
     /**
      * @Author: jingyan
-     * @Time: 2017/7/27 13:48
-     * @Describe: 生成文件
+     * @Time: 2017/9/6 18:20
+     * @Describe: 创建并写入文件
      */
-    public static String createFile(String bodyStr, String filePath, String fileName) {
-        // 拼接文件完整路径
-        String fullPath = filePath + File.separator + fileName;
-        // 生成json格式文件
+    public static String createAndWriteFile(String filePath, String fileName, String content) {
+        String fullPath = createFile(filePath, fileName);
+        if (PubMethod.isEmpty(fullPath)) {
+            return null;
+        }
+        return writeFileByNio(fullPath, content);
+    }
+
+    /**
+     * @Author: jingyan
+     * @Time: 2017/9/6 16:45
+     * @Describe: 创建文件
+     */
+    public static String createFile(String filePath, String fileName) {
         try {
-            // 保证创建一个新文件
+            String fullPath = filePath + File.separator + fileName;
             File file = new File(fullPath);
             // 如果父目录不存在，创建父目录
             if (!file.getParentFile().exists()) {
@@ -33,15 +42,76 @@ public class FileUtil {
                 file.delete();
             }
             file.createNewFile();
-            // 将格式化后的字符串写入文件
-            Writer write = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
-            write.write(bodyStr);
-            write.flush();
-            write.close();
             return fullPath;
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
+
+    /**
+     * @Author: jingyan
+     * @Time: 2017/9/6 16:48
+     * @Describe: NIO 写入文件
+     */
+    public static String writeFileByNio(String filePath, String content) {
+        FileOutputStream fos = null;
+        FileChannel channel = null;
+        try {
+            fos = new FileOutputStream(filePath, true);
+            channel = fos.getChannel();
+            ByteBuffer buf = ByteBuffer.wrap(content.getBytes());
+            buf.put(content.getBytes());
+            buf.flip();
+            channel.write(buf);
+            return filePath;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != channel) {
+                    channel.close();
+                }
+                if (null != fos) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @Author: jingyan
+     * @Time: 2017/9/6 17:54
+     * @Describe: 读取文件（50M 以下 300ms 以内）
+     */
+    public static String readFileByNio(String filePath) {
+        FileInputStream fis = null;
+        FileChannel channel = null;
+        try {
+            fis = new FileInputStream(filePath);
+            channel = fis.getChannel();
+            ByteBuffer buffer = ByteBuffer.allocate((int) channel.size());
+            channel.read(buffer);
+            buffer.flip();
+            String content = new String(buffer.array(), "utf-8");
+            buffer.clear();
+            return content;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                channel.close();
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
 }
