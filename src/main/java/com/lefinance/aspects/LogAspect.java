33715b8ed56886389b3cc47c85237eb9dbb70c5b
Yan <562872237@qq.com>
2017-11-28 12:07:07 +0800

    日志切面

--- src/main/java/com/lefinance/aspects/LogAspect.java
+++ src/main/java/com/lefinance/aspects/LogAspect.java
@@ -1,5 +1,6 @@
 package com.lefinance.aspects;
 
+import com.alibaba.fastjson.JSONObject;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
@@ -13,8 +14,9 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author: Jingyan
@@ -26,48 +28,57 @@ import java.util.Enumeration;
 @Order(-5)
 public class LogAspect {
 
-    private Logger logger =  LoggerFactory.getLogger(this.getClass());
+    private static Logger logger = LoggerFactory.getLogger(LogAspect.class);
+    private static final String POINT_CUT = "execution(public * com.gofun.*.controller..*.*(..))";
 
     /**
-     * 定义一个切入点.
-     * 解释下：
-     *
-     * ~ 第一个 * 代表任意修饰符及任意返回值.
-     * ~ 第二个 * 任意包名
-     * ~ 第三个 * 代表任意方法.
-     * ~ 第四个 * 定义在web包或者子包
-     * ~ 第五个 * 任意方法
-     * ~ .. 匹配任意数量的参数.
+     * Created with Jingyan
+     * Time: 2017-11-24 17:56
+     * Description:  日志打印切点
+     * <p>
+     * 定义一个切入点:
+     * 第一个 * 代表任意修饰符及任意返回值.
+     * 第二个 * 任意包名.
+     * 第三个 * 代表任意方法.
+     * 第四个 * 定义在web包或者子包.
+     * 第五个 * 任意方法.
+     * 最后   .. 匹配任意数量的参数.
      */
-    @Pointcut("execution(public * com.lefinance.regulation.service..*.*(..))")
-    public void webLog(){
+    @Pointcut(POINT_CUT)
+    public void webLog() {
 
     }
 
+    /**
+     * Created with Jingyan
+     * Time: 2017-11-24 16:30
+     * Description: 入参
+     */
     @Before("webLog()")
-    public void doBefore(JoinPoint joinPoint){
-
-        // 接收到请求，记录请求内容
-        logger.info("WebLogAspect.doBefore()");
+    public void doBefore(JoinPoint joinPoint) {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
-        // 记录下请求内容
-        logger.info("URL : " + request.getRequestURL().toString());
-        logger.info("HTTP_METHOD : " + request.getMethod());
-        logger.info("IP : " + request.getRemoteAddr());
-        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() +"." + joinPoint.getSignature().getName());
-        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
-        //获取所有参数方法一：
-        Enumeration<String> enu=request.getParameterNames();
-        while(enu.hasMoreElements()){
-            String paraName=(String)enu.nextElement();
-            System.out.println(paraName+": "+request.getParameter(paraName));
+        Enumeration<String> paramNames = request.getParameterNames();
+        Map<String, Object> map = new HashMap<>();
+        while (paramNames.hasMoreElements()) {
+            String paraName = paramNames.nextElement();
+            map.put(paraName, request.getParameter(paraName));
         }
+        logger.debug(" method={} 入参：, param={}", joinPoint.getSignature().getDeclaringTypeName()
+                + "."
+                + joinPoint.getSignature().getName(), JSONObject.toJSONString(map));
     }
 
-    @AfterReturning("webLog()")
-    public void  doAfterReturning(JoinPoint joinPoint){
-        // 处理完请求，返回内容
-        logger.info("WebLogAspect.doAfterReturning()");
+    /**
+     * Created with Jingyan
+     * Time: 2017-11-24 16:30
+     * Description: 出参
+     */
+    @AfterReturning(value = "webLog()", returning = "retVal")
+    public void doAfterReturning(JoinPoint joinPoint, Object retVal) {
+        logger.debug(" method={} 出参：, param={}", joinPoint.getSignature().getDeclaringTypeName()
+                + "."
+                + joinPoint.getSignature().getName(), JSONObject.toJSONString(retVal));
     }
+
 }
